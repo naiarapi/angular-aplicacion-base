84819c33d65c13804d29cce0d586056c75c29588
Javier Lete <jglete@eskura21.com>
2023-02-07 20:07:23 +0100

    Display a selection list

--- src/app/componentes/heroes/heroes.component.ts
+++ src/app/componentes/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/modelos/hero';
+import { HEROES } from 'src/app/modelos/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,16 @@ import { Hero } from 'src/app/modelos/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+  heroes: Hero[] = HEROES;
+
+  selectedHero?: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
